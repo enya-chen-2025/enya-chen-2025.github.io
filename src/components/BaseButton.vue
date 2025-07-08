7d33554a512d50f4867d98cbd6ba3077975425c1
Enya Chen <enya.chen@104.com.tw>
2025-07-08 16:46:54 +0800

    fix component

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,59 @@
+<template>
+  <button
+    class="button"
+    :class="{
+        'button--green': ButtonColor.Green === backgroundColor,
+        'button--white': ButtonColor.White === backgroundColor,
+    }"
+    @click="onClick"
+  >
+    {{ text }}
+  </button>
+</template>
+<script>
+export const ButtonEvent = Object.freeze({
+    Click: 'click'
+})
+
+export const ButtonColor = Object.freeze({
+    White: 'white',
+    Green: 'green'
+})
+
+</script>
+<script setup>
+
+const props = defineProps({
+    text:{
+        type:String,
+        default : "",
+    },
+
+    backgroundColor: {
+        type: String,
+        default: ButtonColor.White
+    }
+})
+
+const emit = defineEmits([ButtonEvent.Click])
+
+function onClick(event){
+   emit(ButtonEvent.Click, event)
+}
+
+</script>
+
+<style scoped>
+.button {
+ width: 100%;
+}
+
+.button--green {
+    background-color: aquamarine;
+}
+
+.button--white {
+    background-color: white;
+}
+
+</style>
---
+++ src/components/CardView.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="card">
+    <img :src="src" class="card-img-top" />
+    <div class="card-body">
+      <h4>請選擇正確答案</h4>
+      <RadioButton :selected="selected" :options="options" />
+    </div>
+  </div>
+</template>
+
+<script>
+import RadioButton from "@/components/RadioButton.vue";
+
+export default {
+  name: "CardView",
+  components: { RadioButton },
+  props: {
+    //src from Question page//
+    src: {
+      type: String,
+      required: true,
+    },
+    //selected from Question page//
+    selected: {
+      type: String,
+      required: true,
+    },
+    options: {
+      type: Array,
+      required: true,
+    },
+    // index: {
+    //   type: Number,
+    //   required: true,
+    // },
+  },
+};
+</script>
+
+<style>
+.card {
+  width: 14rem;
+  height: 21rem;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  background-color: white;
+  border: 1px solid gainsboro;
+  border-radius: 0.25rem;
+}
+
+.card-img-top {
+  width: 90%;
+  height: 90%;
+  border-radius: 0.25rem;
+  margin: auto;
+  display: block;
+  aspect-ratio: 1 / 1;
+}
+</style>
