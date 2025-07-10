33aee279e3afd59c6749b51346d72a88c5fff6fa
Enya Chen <enya.chen@104.com.tw>
2025-07-10 13:53:05 +0800

    fix button

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -2,8 +2,9 @@
   <button
     class="button"
     :class="{
-        'button--green': ButtonColor.Green === backgroundColor,
-        'button--white': ButtonColor.White === backgroundColor,
+      'button--green': ButtonColor.Green === backgroundColor,
+      'button--white': ButtonColor.White === backgroundColor,
+      'button--red': ButtonColor.Red === backgroundColor,
     }"
     @click="onClick"
   >
@@ -12,48 +13,60 @@
 </template>
 <script>
 export const ButtonEvent = Object.freeze({
-    Click: 'click'
-})
+  Click: "click",
+});
 
 export const ButtonColor = Object.freeze({
-    White: 'white',
-    Green: 'green'
-})
-
+  White: "white",
+  Green: "green",
+  Red: "red",
+});
 </script>
 <script setup>
-
 const props = defineProps({
-    text:{
-        type:String,
-        default : "",
-    },
+  text: {
+    type: String,
+    default: "",
+  },
 
-    backgroundColor: {
-        type: String,
-        default: ButtonColor.White
-    }
-})
+  backgroundColor: {
+    type: String,
+    default: ButtonColor.White,
+  },
 
-const emit = defineEmits([ButtonEvent.Click])
+  data: {
+    type: [String, Number],
+    default: "",
+  },
+});
 
-function onClick(event){
-   emit(ButtonEvent.Click, event)
+const emit = defineEmits([ButtonEvent.Click]);
+
+function onClick(event) {
+  emit(ButtonEvent.Click, event, props.data);
 }
-
 </script>
 
 <style scoped>
 .button {
- width: 100%;
+  width: 10rem;
+  margin-top: 5px;
+  border: 1px solid gainsboro;
+}
+
+.button:hover {
+  border-color: #646cff;
 }
 
 .button--green {
-    background-color: aquamarine;
+  background-color: aquamarine;
 }
 
 .button--white {
-    background-color: white;
+  background-color: white;
 }
 
+.button--red {
+  background-color: red;
+}
 </style>
