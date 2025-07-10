9c8ef842d89c9bcb757ccc58093d44a586a74573
Enya Chen <enya.chen@104.com.tw>
2025-07-10 13:53:34 +0800

    fix card_btn

--- src/components/CardView.vue
+++ src/components/CardView.vue
@@ -1,27 +1,41 @@
 <template>
   <div class="card">
-    <img :src="src" class="card-img-top" />
-    <div class="card-body">
+    <div class="card-img">
+      <img :src="src" class="card-img-top" />
+    </div>
+    <div v-show="ifShow" class="card-body">
       <h4>請選擇正確答案</h4>
-      <RadioButton :selected="selected" :options="options" />
+    </div>
+    <div v-for="option in options">
+      <BaseButton
+        :text="option.value"
+        :data="option.value"
+        :background-color="
+          currentAns === option.value ? ButtonColor.White : ButtonColor.Green
+        "
+        @click="onClick"
+      />
     </div>
   </div>
 </template>
 
 <script>
-import RadioButton from "@/components/RadioButton.vue";
+import BaseButton, { ButtonColor } from "@/components/BaseButton.vue";
 
 export default {
   name: "CardView",
-  components: { RadioButton },
+  components: { BaseButton },
+  data() {
+    return {
+      ButtonColor,
+      currentAns: "",
+      ansList: [],
+      optionIndex: 0,
+    };
+  },
   props: {
-    //src from Question page//
+    // from Question page//
     src: {
-      type: String,
-      required: true,
-    },
-    //selected from Question page//
-    selected: {
       type: String,
       required: true,
     },
@@ -29,10 +43,38 @@ export default {
       type: Array,
       required: true,
     },
-    // index: {
-    //   type: Number,
-    //   required: true,
-    // },
+    name: {
+      type: String,
+      required: true,
+    },
+    ifShow: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  created() {
+    this.ansList = this.$store.state.ansList;
+  },
+  methods: {
+    onClick(e, ans) {
+      if (this.currentAns == ans) {
+        this.currentAns = "";
+      } else {
+        this.currentAns = ans;
+
+        this.optionIndex = this.options.findIndex(
+          (option) => option.value === this.currentAns
+        );
+        console.log("index:", this.optionIndex);
+
+        this.$store.commit("answerList/addList", {
+          value: this.currentAns,
+          img: this.src,
+          label: this.options[this.optionIndex].label,
+          isAns: this.options[this.optionIndex].isAns,
+        });
+      }
+    },
   },
 };
 </script>
@@ -40,7 +82,6 @@ export default {
 <style>
 .card {
   width: 14rem;
-  height: 21rem;
   position: relative;
   display: flex;
   flex-direction: column;
@@ -49,12 +90,20 @@ export default {
   border-radius: 0.25rem;
 }
 
+.card-img {
+  aspect-ratio: 1/1;
+  width: 100%;
+  height: 240px;
+  overflow: hidden;
+}
+
 .card-img-top {
-  width: 90%;
-  height: 90%;
+  width: 100%;
+  height: 100%;
   border-radius: 0.25rem;
   margin: auto;
   display: block;
-  aspect-ratio: 1 / 1;
+  object-fit: cover;
+  object-position: center center;
 }
 </style>
