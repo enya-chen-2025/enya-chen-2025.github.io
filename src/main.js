007ff6b996ce8d5ddf4cc568beebbe94f68d0814
Enya Chen <enya.chen@104.com.tw>
2025-07-17 12:22:38 +0800

    fix

--- src/main.js
+++ src/main.js
@@ -3,10 +3,5 @@ import './style.css'
 import App from './layout/App.vue';
 import router from './router/index.js'
 import store from './store/index.js'
-import '@ungap/global-this';
-
-if (typeof window === 'undefined') {
-  globalThis.crypto = require('crypto').webcrypto;
-}
 
 createApp(App).use(router).use(store).mount('#app')
